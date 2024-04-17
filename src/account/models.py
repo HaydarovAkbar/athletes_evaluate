e19e2c43c479e9511025d9d78cab90375501b767
Akbar Haydarov <81344759+HaydarovAkbar@users.noreply.github.com>
2024-04-17 09:56:53 +0500

    change RefereeUser user model

--- src/account/models.py
+++ src/account/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import AbstractUser, Group, Permission
 from phonenumber_field.modelfields import PhoneNumberField
 
 from app.models import Ring
+
 import uuid
 
 
@@ -33,35 +34,37 @@ class User(AbstractUser):
             models.Index(fields=['username', 'phone_number', 'email']),
         ]
 
-class RefereeUser(AbstractUser):
-    ring=models.ForeignKey(Ring, on_delete=models.SET_NULL, null=True, verbose_name=_('Ring'))
-    main=models.BooleanField(default=False)
-    is_referee=models.BooleanField(default=True)
 
-    created_at=models.DateTimeField(auto_now_add=True)
-    updated_at=models.DateTimeField(null=True)
+class RefereeUser(AbstractUser):
+    ring = models.ForeignKey(Ring, on_delete=models.SET_NULL, null=True, verbose_name=_('Ring'))
+    main = models.BooleanField(default=False)
+    is_referee = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(null=True)
 
     groups = models.ManyToManyField(
-            Group,
-            verbose_name=_('groups'),
-            blank=True,
-            help_text=_(
-                'The groups this user belongs to. A user will get all permissions granted to each of their groups.'),
-            related_name="%(app_label)s_%(class)s_related",  # Change the related_name
-            related_query_name="%(app_label)s_%(class)ss",
-        )
-    permessions=models.ManyToManyField(Permission,verbose_name=_('permessions'),
-            blank=True,
-            help_text=_(
-                'The permissions this user belongs to. A user will get all permissions granted to each of their permissions.'),
-            related_name="%(app_label)s_%(class)s_related",  # Change the related_name
-            related_query_name="%(app_label)s_%(class)ss",
-        )
+        Group,
+        verbose_name=_('groups'),
+        blank=True,
+        help_text=_(
+            'The groups this user belongs to. A user will get all permissions granted to each of their groups.'),
+        related_name="%(app_label)s_%(class)s_related",  # Change the related_name
+        related_query_name="%(app_label)s_%(class)ss",
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        verbose_name=_('user permissions'),
+        blank=True,
+        help_text=_('Specific permissions for this user.'),
+        related_name="%(app_label)s_%(class)s_related",  # Change the related_name
+        related_query_name="%(app_label)s_%(class)ss",
+    )
 
     class Meta:
-        verbose_name='Hakam'
-        verbose_name_plural='Hakamlar'
-        db_table='referee_user'
-        indexes=[
+        verbose_name = 'Hakam'
+        verbose_name_plural = 'Hakamlar'
+        db_table = 'referee_user'
+        indexes = [
             models.Index(fields=['ring']),
         ]
--- src/app/models.py
+++ src/app/models.py
@@ -25,7 +25,7 @@ class Competition(models.Model):
 
 
 class Ring(models.Model):
-    title=models.CharField(max_length=20, null=True)
+    title = models.CharField(max_length=20, null=True)
     competition = models.ForeignKey(Competition, on_delete=models.SET_NULL, null=True, verbose_name=_("Competition"))
 
     is_active = models.BooleanField(default=True, verbose_name=_("Is Active"))
@@ -46,19 +46,17 @@ class Ring(models.Model):
         return str(self.created_at)
 
 
-
 class Match(models.Model):
-    user1=models.CharField(max_length=150, null=True, verbose_name=_('raqib 1'))
-    user2=models.CharField(max_length=150, null=True, verbose_name=_('raqib 2'))
+    user1 = models.CharField(max_length=150, null=True, verbose_name=_('raqib 1'))
+    user2 = models.CharField(max_length=150, null=True, verbose_name=_('raqib 2'))
     ring = models.ForeignKey(Ring, on_delete=models.SET_NULL, null=True)
 
-    result=models.PositiveIntegerField(null=True)
+    result = models.PositiveIntegerField(null=True)
 
     is_finished = models.BooleanField(default=False)
 
     created_at = models.DateTimeField(auto_now_add=True, verbose_name=_("Created At"))
     updated_at = models.DateTimeField(auto_now=True, verbose_name=_("Updated At"))
-
 
     class Meta:
         verbose_name = _("Match")
@@ -74,26 +72,25 @@ class Match(models.Model):
 
 
 class MatchResult(models.Model):
-    ko_choice=(
-        (1,'user1'),
-        (2,'user2'),
+    ko_choice = (
+        (1, 'user1'),
+        (2, 'user2'),
     )
 
-    user1_point=models.CharField(max_length=500, null=True)
-    user2_point=models.CharField(max_length=500, null=True)
-    match=models.OneToOneField(Match, on_delete=models.SET_NULL, null=True)
+    user1_point = models.CharField(max_length=500, null=True)
+    user2_point = models.CharField(max_length=500, null=True)
+    match = models.OneToOneField(Match, on_delete=models.SET_NULL, null=True)
 
-    total_point1=models.PositiveIntegerField(null=True)
-    total_point2=models.PositiveIntegerField(null=True)
-    
-    is_fineshed=models.BooleanField(default=False)
+    total_point1 = models.PositiveIntegerField(null=True)
+    total_point2 = models.PositiveIntegerField(null=True)
 
-    knock_out=models.CharField(max_length=10, choices=ko_choice, null=True)
+    is_fineshed = models.BooleanField(default=False)
+
+    knock_out = models.CharField(max_length=10, choices=ko_choice, null=True)
+
     class Meta:
-        verbose_name='match_result'
-        verbose_name_plural='match_results'
-
+        verbose_name = 'match_result'
+        verbose_name_plural = 'match_results'
 
     def __str__(self):
         return f"{self.match.user1} vs {self.match.user2}'s result"
-    

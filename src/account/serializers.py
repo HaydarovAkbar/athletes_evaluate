087a5081d25d234ae49755666a33427d4fdafe92
Akbar Haydarov <81344759+HaydarovAkbar@users.noreply.github.com>
2024-04-22 11:29:58 +0500

    change coide ...

--- src/account/serializers.py
+++ src/account/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from .models import User
 
+
 class LogInSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
         attrs = super().validate(attrs)
@@ -9,7 +10,7 @@ class LogInSerializer(TokenObtainPairSerializer):
             raise serializers.ValidationError("You are not allowed to login")
         # attrs["organization"] = self.user.organization.title
         attrs["full_name"] = self.user.first_name + " " + self.user.last_name
-        attrs["phone_number"]=self.user.phone_number
+        attrs["phone_number"] = self.user.phone_number
         attrs["groups"] = [group.name for group in self.user.groups.all()]
         attrs["permissions"] = [permission.name for permission in self.user.user_permissions.all()]
         return attrs
@@ -17,9 +18,3 @@ class LogInSerializer(TokenObtainPairSerializer):
     class Meta:
         model = User
         fields = ['username', 'password']
-
-# class RefereeUserSerializers(TokenObtainPairSerializer):
-#     class Meta:
-#         model = RefereeUser
-#         fields = ['username', 'password']
-
